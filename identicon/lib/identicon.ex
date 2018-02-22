ef4df07da16d5d18f777ebbfc55628090552a61d
ivoferroo <1151159@isep.ipp.pt>
2018-02-22 23:32:23 +0000

    starts identicon project

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,35 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+end
