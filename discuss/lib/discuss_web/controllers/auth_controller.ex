b50b65b81229913961d32a1efb25fe76da3142b2
ivoferro <ivoferro1995@gmail.com>
2018-03-18 21:59:07 +0000

    Implements sign in with github

---
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -0,0 +1,23 @@
+defmodule DiscussWeb.AuthController do
+  use DiscussWeb, :controller
+  plug Ueberauth
+
+  alias Discuss.Accounts
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider}) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: provider}
+
+    case Accounts.get_or_create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
+  end
+
+end
